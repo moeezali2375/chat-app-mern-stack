6f301268c56bd81282f6db087dbc41088aa1ff67
moeezali <moeezali2375@gmail.com>
2024-02-05 14:24:20 +0500

    add backend endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const { chats } = require("./data/data");
 const app = express();
 
 const PORT = process.env.PORT;
@@ -8,7 +9,15 @@ app.get("/", (req, res) => {
 	res.send("API is up and running!");
 });
 
+app.get("/api/chat", (req, res) => {
+	res.send(chats);
+});
 
+app.get("/api/chat/:id", () => {
+	const id = req.params.id;
+	const singleChat = chats.find((c) => c._id === req.params.id);
+	res.send(singleChat);
+});
 app.listen(PORT, () => {
 	console.log("Server Started on Port: ", PORT);
 });
