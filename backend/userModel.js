44cf6f985ff3e8b2009d6e0ef3dc82e650ed0f76
moeezali <moeezali2375@gmail.com>
2024-02-06 21:20:30 +0500

    add models

---
+++ backend/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const User = mongoose.model("User", userModel);
+
+module.exports = User;
