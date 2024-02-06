c140710c2650cbb7d89f5663e6ded50592598ae5
moeezali <moeezali2375@gmail.com>
2024-02-06 21:21:21 +0500

    HomePage add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
 
 function App() {
-	return (
-		<div className="App">
-			<Route path="/" component={HomePage} exact/>
-			<Route path="/chats" component={ChatPage} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Pages/homePage.js
+++ frontend/src/Pages/homePage.js
@@ -1,7 +1,57 @@
 import React from "react";
-
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
 const HomePage = () => {
-	return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work sans"
+          color="black"
+          textAlign={"center"}
+        >
+          Jutt Chats
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth={"1px"}
+      >
+        <Tabs size="md" variant="enclosed">
+          <TabList mb="1">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>{/* <p>one!</p> */}</TabPanel>
+            <TabPanel>{/* <p>two!</p> */}</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
