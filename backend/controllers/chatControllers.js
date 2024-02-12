81548921ea10b72d62bc549650f6ecd621c40a88
moeezali <moeezali2375@gmail.com>
2024-02-12 23:21:08 +0500

    hehe

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -63,7 +63,40 @@ const fetchChats = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
-const createGroupChat = asyncHandler(async (req, res) => {});
+const createGroupChat = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please fill all the fields" });
+  }
+
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send({ message: "More than 2 users are required to form a group chat" });
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user._id,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 const renameGroup = asyncHandler(async (req, res) => {});
 const removeFromGroup = asyncHandler(async (req, res) => {});
 const addToGroup = asyncHandler(async (req, res) => {});
