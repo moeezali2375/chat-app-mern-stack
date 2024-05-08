7d1ebcb22139dd2af1207b58d91da72d705bca9f
moeezali <moeezali2375@gmail.com>
2024-05-08 06:38:28 +0500

    AUTH BUG FIX 🤨

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -11,7 +11,11 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
+      const res = await User.findById(decoded.id).select("-password");
+      if (!res) {
+        throw new Error("Not authorized");
+      }
+      req.user=res;
       next();
     } catch (error) {
       res.status(401);
@@ -24,8 +28,4 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = {protect};
-
-
-
-
+module.exports = { protect };
